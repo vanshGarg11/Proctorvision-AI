831960887da620dbe8d71617f028e6e7e352717c
Vansh Garg <vanshgarg805@gmail.com>
2026-05-21 18:38:13 +0530

    Connect frontend to ngrok backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 api.interceptors.request.use((config) => {
